d56318d60fa79f019e0d929c53f53a786792fb93
Eric Wagner <eric.wagner@kapioshealth.com>
2018-06-06 16:56:11 -0400

    Save

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 import { Router } from '@angular/router';
+import { OverlayContainer } from '@angular/cdk/overlay';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
     title = 'app';
+    @ViewChild("wrapper", {read: ElementRef}) wrapper: ElementRef;
 
     constructor(
-        public router: Router
-    ) {}
+        public router: Router,
+        public overlayContainer: OverlayContainer
+    ) {
+    }
+
+    ngOnInit(): void {
+        var themeId = localStorage.getItem('themeId');
+        if (+themeId === 1) {
+            this.wrapper.nativeElement.className = "kapios-theme--dark";
+            this.overlayContainer.getContainerElement().classList.add("kapios-theme--dark");
+        } else {
+            this.wrapper.nativeElement.className = "kapios-theme--light";
+            this.overlayContainer.getContainerElement().classList.add("kapios-theme--light");
+        }
+
+    }
 
     isAuthenticated(): boolean {
         return this.router.url !== '/login';
+    }
+
+    changeTheme(theme: string): void {
+        this.wrapper.nativeElement.className = theme;
+        this.overlayContainer.getContainerElement().className = "cdk-overlay-container " + theme; 
     }
 }
--- src/app/shared/layout/header.component.ts
+++ src/app/shared/layout/header.component.ts
@@ -1,11 +1,36 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, Output, EventEmitter, ElementRef, ViewEncapsulation } from '@angular/core';
 
 @Component({
     selector: 'layout-header',
     templateUrl: './header.component.html',
-    styleUrls: ['./layout.css']
+    encapsulation: ViewEncapsulation.None
 })
 export class HeaderComponent implements OnInit {
+    @ViewChild("themer", {read: ElementRef}) themer: ElementRef;
+    @Output() currentTheme = new EventEmitter(); 
+    theme: string;
+
     constructor() {}
-    ngOnInit() {}
+    ngOnInit() {
+        this.theme = "kapios-theme--light"; 
+    }
+
+    setTheme(id: number): void {
+        localStorage.setItem('themeId', id.toString());
+        switch (id) {
+            case 1: {
+                this.theme = "kapios-theme--dark";  
+                this.themer.nativeElement.classList.remove("toolbar-themer--light");
+                this.themer.nativeElement.classList.add("toolbar-themer--dark");
+                break;
+            } 
+            default: {
+                this.theme = "kapios-theme--light";
+                this.themer.nativeElement.classList.remove("toolbar-themer--dark");
+                this.themer.nativeElement.classList.add("toolbar-themer--light");
+                break;
+            }
+        }
+        this.currentTheme.emit(this.theme);
+    }
 }
